864bf78118eb0bb5922debfce24401c8d500a69c
Robert <rjaroca@corhuila.edu.co>
2022-02-11 09:47:30 -0500

    Iniciamos el desarrollo del juego de ping pong.

---
+++ main.js
@@ -0,0 +1,41 @@
+(function(){
+    var selfBoard = function(width, height){
+        this.width = this.width;
+        this.height = this.height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+    }
+
+    self.Board.prototype ={
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+            return elements;
+
+        }
+
+    }
+})();
+(function main(){
+    self.BoardView = function(canvas, board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+
+    }
+
+})();
+
+self.addEventListener("load", main);
+
+function main(){
+
+    var board = new Board(800,400);
+    console.log(board);
+    var canvas = document.getElementsById("canvas");
+    var board_view = new BoardView(canvas,board); 
+}
