bf08d9397d25b205ca263cf7f9acc13425c89b77
Robert <rjaroca@corhuila.edu.co>
2022-02-11 22:48:16 -0500

    se le da movimiento y velocidad a la pelota.

--- main.js
+++ main.js
@@ -6,11 +6,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing= false;
     }
 
     self.Board.prototype={
         get elements(){
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball);
             return elements;
 
@@ -27,11 +28,20 @@
         this.speed_y =0;
         this.speed_x = 3;
         this.board= board;
+        this.direction = 1;
 
         board.ball= this;
         this.kind="circle";
+
     }
-}
+    self.Ball.prototype={
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        }
+    }
+})();
 
 (function(){
     self.Bar = function(x,y,width,height,board){
@@ -61,7 +71,7 @@ self.Bar.prototype={
 
     }
 }
-})(); 
+})();
 
 (function main(){
     self.BoardView = function(canvas, board){
@@ -86,8 +96,13 @@ self.Bar.prototype={
             };
         },
         play: function(){
+            if(this.board.playing){
             this.clean();
             this.draw();
+            this.board.ball.move();
+
+            }
+            
         }
     }
 
@@ -121,9 +136,10 @@ var ball = new Ball (350,100,10,board);
 
 
 document.addEventListener("keydown", function(ev){
-    ev.preventDefault();
+    
   
     if(ev.keycode== 38){
+        ev.preventDefault();
         bar.up();
     }
     else if(ev.keycode == 40){
@@ -134,11 +150,16 @@ document.addEventListener("keydown", function(ev){
     }else if (ev.keycode ===83){
     //S
     bar_2.down();
+    }else if(ev.keycode===32){
+        ev.preventDefault();
+        board.playing= !board.playing;
+
     }
 
 });
-
+board_view.draw();
 window.requestAnimationFrame(controller);
+
 
 function controller(){
 
